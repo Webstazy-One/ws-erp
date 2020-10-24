31da2cef07452b85e8199fc508f3eb9af623b454
Webstazy-One <io@webstazy.one>
2020-10-24 21:05:26 +0530

    pipeline test

--- server.js
+++ server.js
@@ -107,7 +107,7 @@ app.get("/*", (req, res) => {
  res.sendFile(__dirname + '/dist/index.html')
 })
 
-const PORT = process.env.PORT || 8090;
+const PORT = process.env.PORT || 8089
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`)
 })
