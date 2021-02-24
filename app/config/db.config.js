0770db28bde22cda2cf4cbd384e3f884d7c071b8
Aaqib Webstazy <aaqib.wiki@webstazy.one>
2021-02-24 16:48:02 +0530

    Update Dev DbUrl

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,14 +1,15 @@
-  module.exports = {
-      url: "mongodb+srv://harsha:WVrb2YigY80G9LX0@cluster0.cpmky.gcp.mongodb.net/wipnew?retryWrites=true&w=majority",
-      //"mongodb+srv://testuser:lY3PBjnn4fJpJxPx@mongalbavina.dut54.mongodb.net/bavifire?retryWrites=true&w=majority",
+module.exports = {
+  // url: "mongodb+srv://harsha:WVrb2YigY80G9LX0@cluster0.cpmky.gcp.mongodb.net/wipnew?retryWrites=true&w=majority",
+  //url: "mongodb+srv://webstazyOneDev:dFfLfSzn9p3p2Z5Y@cluster0.zupsa.mongodb.net/jobsy?retryWrites=true&w=majority",
+  //url: "mongodb+srv://testuser:lY3PBjnn4fJpJxPx@mongalbavina.dut54.mongodb.net/bavifire?retryWrites=true&w=majority",
+  url: "mongodb+srv://dev:daLsxN106c279J8r@cluster0.cpmky.gcp.mongodb.net/wipnew?retryWrites=true&w=majority",
+
+  //serverUrl: "http://wimsons011.eba-yicete3g.ap-southeast-1.elasticbeanstalk.com",
+  // "http://localhost:8089",
 
 
-      serverUrl: "http://wimsons011.eba-yicete3g.ap-southeast-1.elasticbeanstalk.com",
-      // "http://localhost:8089",
+  //OGFSLRewardUrl: "https://dev.1galleface.com"
 
+  //"https://dev.1galleface12.com"
 
-      OGFSLRewardUrl: "https://dev.1galleface.com"
-
-      //"https://dev.1galleface12.com"
-
-  }
+}
