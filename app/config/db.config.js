1cdf67156320013b0e26792b7caeba12b38618e5
Aaqib <78718007+aaqib-webstazy@users.noreply.github.com>
2021-03-05 11:43:41 +0530

    Update Dev Url

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,7 +1,7 @@
   module.exports = {
-      url: "mongodb+srv://dev:daLsxN106c279J8r@cluster0.cpmky.gcp.mongodb.net/wipnew?retryWrites=true&w=majority",
+      //url: "mongodb+srv://dev:daLsxN106c279J8r@cluster0.cpmky.gcp.mongodb.net/wipnew?retryWrites=true&w=majority",
       //"mongodb+srv://testuser:lY3PBjnn4fJpJxPx@mongalbavina.dut54.mongodb.net/bavifire?retryWrites=true&w=majority",
-
+      url: "mongodb+srv://wimsons-dev:qLYBZKcVvRRsBM8f@cluster0.k549k.mongodb.net/ws-erp-live?retryWrites=true&w=majority",
 
       serverUrl: "http://wimsons011.eba-yicete3g.ap-southeast-1.elasticbeanstalk.com",
       // "http://localhost:8089",
